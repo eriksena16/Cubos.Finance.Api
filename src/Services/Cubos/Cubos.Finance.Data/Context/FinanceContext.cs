6a8a8adcf6b4567357a942fb8d6a15d9cee05cb5
Erik Sena <eriksena16@gmail.com>
2025-07-31 19:01:33 -0300

    Feat: Data structure initial

---
+++ src/Services/Cubos/Cubos.Finance.Data/Context/FinanceContext.cs
@@ -0,0 +1,57 @@
+﻿using Cubos.Finance.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cubos.Finance.Data
+{
+    public class FinanceContext : DbContext
+    {
+        public FinanceContext(DbContextOptions<FinanceContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<People> People { get; set; }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+                .Properties<string>()
+                .AreUnicode(false)
+                .HaveMaxLength(100);
+
+            configurationBuilder
+                .Properties<decimal>()
+                .HavePrecision(18, 2);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FinanceContext).Assembly);
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.State != EntityState.Modified)
+                    continue;
+
+                if (entry.Properties.Any(p => p.Metadata.Name == "UpdatedAt"))
+                {
+                    entry.Property("UpdatedAt").CurrentValue = DateTime.UtcNow;
+                }
+
+                if (entry.Properties.Any(p => p.Metadata.Name == "CreatedAt"))
+                {
+                    entry.Property("CreatedAt").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Cubos/Cubos.Finance.Data/Repositories/PeopleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cubos.Finance.Data
+{
+    public class PeopleRepository
+    {
+    }
+}
