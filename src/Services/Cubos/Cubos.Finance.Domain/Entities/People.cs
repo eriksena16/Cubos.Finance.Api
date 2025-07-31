3a41af3b345ee92422faffcf6cfcb7343ca6bb2c
Erik Sena <eriksena16@gmail.com>
2025-07-31 19:01:44 -0300

    Feat: Domain structure initial

---
+++ src/Services/Cubos/Cubos.Finance.Domain/Entities/People.cs
@@ -0,0 +1,13 @@
+﻿using Cubos.Finance.Shared;
+
+namespace Cubos.Finance.Domain
+{
+    public class People : Entity
+    {
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt => DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+    }
+}
