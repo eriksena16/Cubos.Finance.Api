743b8e0508c20b740b453876385f0d458771e338
Erik Sena <eriksena16@gmail.com>
2025-08-01 16:56:29 -0300

    Feat: endpoint people

---
+++ src/Services/Cubos/Cubos.Finance.Api/Controllers/v1/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Cubos.Finance.Application;
+using Cubos.Finance.Shared;
+
+namespace Cubos.Finance.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PeopleController : ApiControllerBase
+    {
+        private readonly IPeopleService _peopleService;
+        public PeopleController(INotifier notifier, IPeopleService peopleService) : base(notifier)
+        {
+            _peopleService = peopleService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PeopleRequest request)
+        {
+            var response = await _peopleService.CreateAsync(request);
+
+            return response != null
+                ? Ok(response)
+                : BadRequest();
+        }
+
+    }
+}
